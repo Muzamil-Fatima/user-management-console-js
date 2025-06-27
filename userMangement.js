a2c89c7c7fed94e632bd6e0cb22e51cef355727a
Muzamil <muzamilzubair987654@gmail.com>
2025-06-27 09:42:32 +0500

    update user

--- userMangement.js
+++ userMangement.js
@@ -5,11 +5,35 @@ const users = [
   { id: 3, name: "Ahmad" },
 ];
 
+//  Add new user
+
 function addUser(name) {
   const lastUser = users[users.length - 1];
   const getId = lastUser ? lastUser.id + 1 : 1;
-  const newId = { id: getId, name: name };
-  users.push(newId);
+  const newUser = { id: getId, name: name };
+  users.push(newUser);
 }
 addUser("zara");
 console.log(users);
+
+// updateUser
+function updateUser(id, newName) {
+  let found = false;
+  users.forEach((fetchUser) => {
+    if (fetchUser.id === id) {
+      fetchUser.name = newName;
+      found = true;
+    }
+  });
+  if (!found) {
+    console.log("ID does not match ");
+  }
+}
+updateUser(2, "Maria");
+console.log(users);
+
+// listUsers
+
+function listUsers(){
+  
+}
